4169547eaef61933d91798285d1d4d402261fd2b
DarkNio <sonic-xx@hotmail.it>
2023-05-04 12:10:28 +0200

    Create YouTube - Confirm presence.user.js

---
+++ YouTube - Confirm presence.user.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         YouTube - Confirm presence
+// @version      1.1
+// @description  Confirm presence on YouTube automatically
+// @author       DarkNio
+// @match        https://www.youtube.com/watch*
+// @grant        none
+// @namespace    DarkNio.youTubePresence
+// @license      MIT
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    setInterval(function (){
+        //yt-spec-touch-feedback-shape__fill
+        if(document.querySelector('#confirm-button')) {
+            document.querySelector('#confirm-button').click();
+        }
+    }, 2500);
+})();
